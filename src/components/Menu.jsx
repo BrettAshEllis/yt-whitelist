f8786ab09ee61802ce5171c7203a14f292815571
Brett Ellis <brett.ash.ellis@gmail.com>
2022-02-23 16:05:11 -0600

    Boilerplates, scaffolding

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {
+    BrowserRouter as
+        Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+export default Menu;
