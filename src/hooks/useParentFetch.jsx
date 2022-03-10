8d8e30d0ec462bb0e2a7c4ebb8f8d41240cdd626
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-10 16:48:33 -0600

    Started loadData function

--- src/hooks/useParentFetch.jsx
+++ src/hooks/useParentFetch.jsx
@@ -1,14 +1,26 @@
 import { useState, useEffect } from "react"
-import { use }
 
-const baseURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&maxResults=5&key=AIzaSyAnu7Cu2x6E1HbI-sNDkbLrtbbn-oWAJhI&q=`
+const baseURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&maxResults=5&key=AIzaSyAnu7Cu2x6E1HbI-sNDkbLrtbbn-oWAJhI&forUsername=`
 
 export default function useParentFetch(parentSearch) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
 
-    useEffect
+    useEffect(() => {
+        async function loadData() {
+            setLoading(true)
+        };
+        if (parentSearch.length === 0) {
+            setLoading(false);
+            return
+        }
+        setData(null);
+        setError(null);
+        try {
+
+        }
+    })
 
 
 }
