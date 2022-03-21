2b62d1a83de054040fbdea95d9e28d317f85c552
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-21 16:21:51 -0500

    pruned userSearch

--- src/shared/actions/Search.actions.js
+++ src/shared/actions/Search.actions.js
@@ -1,15 +1,16 @@
-export const SET_USER_SEARCH = "Set User Search"
-export const CLEAR_USER_SEARCH = "Clear User Search"
+// export const SET_USER_SEARCH = "Set User Search"
+// export const CLEAR_USER_SEARCH = "Clear User Search"
 export const SET_PARENT_SEARCH = "Set Parent Search"
 export const CLEAR_PARENT_SEARCH = "Clear Parent Search"
 
-export const setUserSearch = (userSearch) => {
-    return { type: SET_USER_SEARCH, userSearch }
-};
 
-export const clearUserSearch = () => {
-    return { type: CLEAR_USER_SEARCH }
-};
+// export const setUserSearch = (userSearch) => {
+//     return { type: SET_USER_SEARCH, userSearch }
+// };
+
+// export const clearUserSearch = () => {
+//     return { type: CLEAR_USER_SEARCH }
+// };
 
 export const setParentSearch = (parentSearch) => {
     return { type: SET_PARENT_SEARCH, parentSearch }
@@ -18,3 +19,4 @@ export const setParentSearch = (parentSearch) => {
 export const clearParentSearch = () => {
     return { type: CLEAR_PARENT_SEARCH }
 };
+
--- src/shared/reducers/Search.Reducer.js
+++ src/shared/reducers/Search.Reducer.js
@@ -1,29 +1,25 @@
 import {
     SET_PARENT_SEARCH,
     CLEAR_PARENT_SEARCH,
-    SET_USER_SEARCH,
-    CLEAR_USER_SEARCH,
+    // SET_USER_SEARCH,
+    // CLEAR_USER_SEARCH,
 
 } from "../actions/Search.actions";
 
 const initialState = {
-    search: {
-        parentSearchState: [],
-        userSearchState: [],
-    }
-
+    search: []
 }
 
 export default function searchReducer(state = initialState, action) {
     switch (action.type) {
         case SET_PARENT_SEARCH:
-            return { ...state, parentSearchState: action.parentSearch };
+            return { ...state, search: action.parentSearch };
         case CLEAR_PARENT_SEARCH:
-            return { ...state, parentSearchState: [] };
-        case SET_USER_SEARCH:
-            return { ...state, userSearchState: action.userSearch };
-        case CLEAR_USER_SEARCH:
-            return { ...state, userSearchState: [] };
+            return { ...state, search: [] };
+        // case SET_USER_SEARCH:
+        //     return { ...state, userSearchState: action.userSearch };
+        // case CLEAR_USER_SEARCH:
+        //     return { ...state, userSearchState: [] };
 
         default:
             return state;
