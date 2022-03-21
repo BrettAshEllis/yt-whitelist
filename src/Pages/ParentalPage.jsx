6e711f5ec74bbc2a3ca21dbb0bc04e3b487b27df
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-21 16:21:17 -0500

    started useEffect in parent Page

--- src/Pages/ParentalPage.jsx
+++ src/Pages/ParentalPage.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { useRef, useState } from "react";
 import { connect } from "react-redux";
-import { whitelistCreator, removeWhitelist, setParentSearch } from "./shared/actions/User.actions"
+import { addWhitelist, removeWhitelist } from "./shared/actions/Whitelist.actions"
 import { setParentSearch } from "../shared/actions/Search.actions"
+import { useEffect } from "react";
+import { fetchChannel } from "../shared/actions";
 
-function ParentalSearch({ setParentSearch, whitelistCreator, removeWhitelist }) {
+function ParentalSearch({ setParentSearch, addWhitelist, removeWhitelist, fetchChannel }) {
 
     const [parentCreatorQuery, setParentCreatorQuery] = useState("");
     const parentCreatorSearch = useRef(null);
-
+    useEffect(() => {
+        fetchChannel()
+    }, [parentCreatorQuery])
 
     return (
         <div>
             <div>
                 <label htmlFor="parentCreatorSearch">Search for a content creator to approve!</label>
-                <input type={Text} placeholder="E.g. PBS Kids" id="parentCreator" ref={parentCreatorSearch} />
+                <input type={Text} placeholder="E.g. Jerma985" id="parentCreator" ref={parentCreatorSearch} />
                 <button onClick={() => { setParentCreatorQuery(parentCreatorSearch.current.value); }}> Search creators! </button>
             </div>
         </div>
