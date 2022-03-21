bc09542415c2a8860a70410836ff3d664c8fa1ad
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-21 09:47:38 -0500

    removed parent video search

--- src/Pages/ParentalPage.jsx
+++ src/Pages/ParentalPage.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { useRef, useState } from "react";
 import { connect } from "react-redux";
 import { whitelistCreator, removeWhitelist, setParentSearch } from "./shared/actions/User.actions"
+import { setParentSearch } from "../shared/actions/Search.actions"
 
 function ParentalSearch({ setParentSearch, whitelistCreator, removeWhitelist }) {
 
     const [parentCreatorQuery, setParentCreatorQuery] = useState("");
-    const [parentVideoQuery, setParentVideoQuery] = useState("");
     const parentCreatorSearch = useRef(null);
-    const parentVideoSearch = useRef(null);
 
 
     return (
@@ -18,23 +17,17 @@ function ParentalSearch({ setParentSearch, whitelistCreator, removeWhitelist }) 
                 <input type={Text} placeholder="E.g. PBS Kids" id="parentCreator" ref={parentCreatorSearch} />
                 <button onClick={() => { setParentCreatorQuery(parentCreatorSearch.current.value); }}> Search creators! </button>
             </div>
-            <br />
-            <div>
-                <label htmlFor="parentVideoSearch">"Search for a video to approve!"</label>
-                <input type={Text} placeholder="E.g. Minecraft Lets Play" id="parentVideo" />
-                <button onClick={() => { setParentVideoQuery(parentVideoSearch.current.value); }}> Search videos! </button>
-            </div>
         </div>
     )
 };
 
 const mapStateToProps {
     return { }
-}
+};
 
 const mapDispatchToProps {
-    setSearch,
-    whitelistCreator,
+    setParentSearch,
+    addWhitelist,
     removeWhitelist
 }
 
