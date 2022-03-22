5a7d611de7bc7a6fb66fdee69f1897a372393942
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-22 09:34:31 -0500

    Added header tag to error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function errorPage() {
     return (
         <div>
-            This shit brokey, go to a navigatable page
+            <h1>This shit brokey and you aint suppossed to be here. Please, go to a navigatable page.</h1>
         </div>
     )
 }
