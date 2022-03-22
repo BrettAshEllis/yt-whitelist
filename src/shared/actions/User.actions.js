93103747431be1a0b233c24814ece17094b446d7
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-22 11:16:53 -0500

    Setting the parent to the parent's username

--- src/shared/actions/User.actions.js
+++ src/shared/actions/User.actions.js
@@ -1,8 +1,8 @@
 export const SET_PARENT = "Set Parent";
 export const CLEAR_PARENT = "Clear Parent";
 
-export const setParent = (parent) => {
-    return { type: SET_PARENT, parent };
+export const setParent = (parentUsername) => {
+    return { type: SET_PARENT, parent: parentUsername };
 };
 
 export const clearParent = () => {
