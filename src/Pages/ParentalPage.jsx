6c789dcb0c74fc0bddb5292a856f923a746da5b6
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-24 16:01:36 -0500

    Added code for useFetch hook, dropping Axios

--- src/Pages/ParentalPage.jsx
+++ src/Pages/ParentalPage.jsx
@@ -2,25 +2,44 @@ import React from "react";
 import { useRef, useState } from "react";
 import { connect } from "react-redux";
 import { addWhitelist, removeWhitelist } from "./shared/actions/Whitelist.actions"
-import { setParentSearch } from "../shared/actions/Search.actions"
+import { setSearch } from "../shared/actions/Search.actions"
 import { useEffect } from "react";
 import { fetchChannel } from "../shared/actions";
+import useParentFetch from "../hooks/useParentFetch";
 
-function ParentalSearch({ setParentSearch, addWhitelist, removeWhitelist, fetchChannel }) {
+function ParentalSearch({ setParentSearch, add, remove, fetchChannel, whitelist, search }) {
 
+    const { data, error, loading } = useParentFetch(parentCreatorQuery);
+    const whitelisted = useMemo(() => whitelist.map((val) => val.channel_id), [whitelist]);
     const [parentCreatorQuery, setParentCreatorQuery] = useState("");
     const parentCreatorSearch = useRef(null);
     useEffect(() => {
-        fetchChannel()
-    }, [parentCreatorQuery])
+        if (data) {
+            setSearch(data)
+        }
+    }, [data, setSearch])
 
     return (
         <div>
             <div>
                 <label htmlFor="parentCreatorSearch">Search for a content creator to approve!</label>
                 <input type={Text} placeholder="E.g. Jerma985" id="parentCreator" ref={parentCreatorSearch} />
-                <button onClick={() => { setParentCreatorQuery(parentCreatorSearch.current.value); }}> Search creators! </button>
+                <button onClick={() => { setParentCreatorQuery(parentCreatorSearch.current.value) }}> Search creators! </button>
             </div>
+            {loading && <div>Loading...</div>}
+            {error && !loading && <h2>Error, request did not finish</h2>}
+            {!error && search && !loading && (
+                <div>
+                    {search.map((val) => (
+                        <ChannelDisplay
+                            key={val.id}
+                            channel={val}
+                            addWhitelist={addWhitelist}
+                            removeWhitelist={removeWhitelist}
+                            isWhitelisted={whitelisted.includes(val.id)} />
+                    ))}
+                </div>
+            )}
         </div>
     )
 };
@@ -28,16 +47,17 @@ function ParentalSearch({ setParentSearch, addWhitelist, removeWhitelist, fetchC
 const mapStateToProps = state => {
     return {
         parent: state.parent,
-        whitelist: state.whitelisted.whitelist,
-        search: state.search.parentSearchState
+        whitelist: state.whitelist,
+        search: state.search,
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        setParentSearch,
+        setSearch,
         add: addWhitelist,
         remove: removeWhitelist,
+        fetchChannel,
     }
 }
 
