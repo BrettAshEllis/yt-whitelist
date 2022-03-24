45e72bcd970a537d92a5705a8aebb4dc97246710
Brett Ellis <brett.ash.ellis@gmail.com>
2022-03-24 14:18:06 -0500

    Changed VideoDisplay to ChannelDisplay

---
+++ src/components/ChannelDisplay.jsx
@@ -0,0 +1,12 @@
+//! Display Channel thumbnail, title, and Description
+//! Add button to add channel to whitelist
+
+import react from "react"
+
+function ChannelDisplay({ channel, addWhitelist, removeWhitelist, isWhitelist }) {
+    return (
+        <div>
+            <h3>{whitelist.</h3>
+        </div>
+    )
+}
